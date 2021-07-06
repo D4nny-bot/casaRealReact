0196ceced1e0719eef32b60c15ffc3f5ad58746f
Deykhon <28.deykhon.08@gmail.com>
2021-07-05 21:32:42 -0400

    login casa real

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import AppContext from "./src/context/AppContext"
+import {createStackNavigator} from "@react-navigation/stack"
+import { NavigationContainer } from '@react-navigation/native';
+import Login from "./src/screens/Login"
+import DataState from "./src/context/AppState";
+import {View} from "react-native";
+import MainApp from './src/screens/MainApp';
+
+var Stack = createStackNavigator();
+class App extends React.Component {
+  static contextType = AppContext;
+  constructor(props: any) {
+    super(props);
+  }
+  render() {
+    return <DataState>
+    <NavigationContainer >
+      <Stack.Navigator >
+      <Stack.Screen name="login"  component={Login} /> 
+        <Stack.Screen name="main" component={MainApp} options={() => (
+              {header: (navigate) => <View></View>}
+            )}/> 
+      </Stack.Navigator>
+    </NavigationContainer>
+    </DataState>
+  }
+}
+
+export default App;
---
+++ src/screens/Login.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import {View, Image, StyleSheet, ImageBackground, Alert} from "react-native";
+import {TextInput, Button} from "react-native-paper";
+import AppContext from "../context/AppContext"
+import { StackNavigationProp } from "@react-navigation/stack";
+import MainApp from './MainApp';
+import { NavigationContainer } from "@react-navigation/native";
+
+
+interface MyState {
+      username: String,
+      password: String,
+      visible: boolean
+}
+interface MyProps {
+    navigation: StackNavigationProp<any, any>
+}
+export interface IGoogleUser {
+  photo: string
+  email: string
+  familyName?: string
+  givenName: string
+  name: string
+  id: string
+}
+class Login extends React.Component<MyProps, any> {
+  static contextType = AppContext;
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      username: "",
+      password: "",
+      visible: true
+    }
+  }
+  login() {
+    
+    if (true) {
+      this.props.navigation.navigate("main");
+    }    
+  
+  }
+  render() {
+    return <ImageBackground style={styles.body} source={require("../../assets/img/fondo.jpg")}>
+        <View style={styles.centerObjects}>
+          <Image style={styles.logo} source={require("../../assets/img/casaReal.jpg")}/>
+        </View>
+        <View style={styles.containerTextfield}>
+          <TextInput
+            label="Username"
+            
+            onChangeText={(text) => {
+              this.setState({
+                username: text
+              })
+            }}
+          />
+          <TextInput style={styles.marginTop}
+            label="Password"
+            secureTextEntry={this.state.visible}
+            right={<TextInput.Icon name="eye" onPress={() => {
+              this.setState({
+                visible: !this.state.visible
+              })
+            }}/>}
+            onChangeText={(text) => {
+              this.setState({
+                password: text
+              })
+            }}
+          />
+          <Button style={styles.marginTop}  mode="contained" onPress ={() => {this.login()}}>
+            Login
+          </Button>
+        
+        </View>
+    </ImageBackground>
+  }
+}
+const styles = StyleSheet.create({
+  body: {
+    flex:1
+  },
+  logo: {
+    width: "50%",
+    height: "50%"
+  },
+  containerTextfield: {
+    padding: 10,
+  },
+  centerObjects: {
+    alignItems: "center"
+  },
+  marginTop: {
+    marginTop: 10
+  }
+});
+export default Login;
